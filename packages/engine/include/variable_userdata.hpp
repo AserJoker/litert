8381919046f0860504f8c7cdc1ff7efbbd33ddd7
w30029682 <wangjinfan1@huawei.com>
2023-10-08 10:00:18 +0800

    #feat userdata

---
+++ packages/engine/include/variable_userdata.hpp
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "core/include/auto_release.hpp"
+#include "variable.hpp"
+namespace litert::engine {
+class variable_userdata : public variable {
+private:
+  core::auto_release<core::object> _data;
+
+public:
+  variable_userdata(context *, const core::auto_release<core::object> &);
+  core::auto_release<core::object> &data();
+  const core::auto_release<core::object> &data() const;
+  bool to_boolean() const override;
+};
+} // namespace litert::engine
---
+++ packages/engine/src/variable_userdata.cc
@@ -0,0 +1,11 @@
+﻿#include "../include/variable_userdata.hpp"
+using namespace litert::engine;
+using namespace litert;
+variable_userdata::variable_userdata(
+    context *ctx, const core::auto_release<core::object> &data)
+    : variable(ctx, VT_USERDATA), _data(data) {}
+core::auto_release<core::object> &variable_userdata::data() { return _data; }
+const core::auto_release<core::object> &variable_userdata::data() const {
+  return _data;
+}
+bool variable_userdata::to_boolean() const { return true; }
